475021c787cbd07f7673b0be8addee4450ed3c28
Elite-tch <elite6461@gmail.com>
2024-10-23 13:31:45 +0100

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import DoctorsProfile from './pages/DoctorsProfile'
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
 
 const App = () => {
   return (
     <div>
-      
-      <Router>
-      <Routes>
-        <Route path="/*" element={<DoctorsProfile />} /> {/* All doctor-related routes */}
-      </Routes>
-    </Router>
+     
     </div>
   )
 }
